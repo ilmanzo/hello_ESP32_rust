20b6c08de2bfa6019d0b7db5a2c6c414deb16cca
Andrea Manzini <ilmanzo@gmail.com>
2022-10-01 14:37:00 +0200

    first build test

---
+++ README.md
@@ -0,0 +1,20 @@
+# hello, ESP
+
+my experiments running Rust on ESP32.
+
+This is my ESP32, found in a drawer taking dust ...
+
+![esp32](img/ESP32.png)
+
+instead of dust, let's make it Rust :D 
+
+
+## useful links:
+
+https://github.com/esp-rs/rust-build
+
+https://github.com/esp-rs/esp-idf-template
+
+
+## Dev Containers ? YES!
+
---
+++ export-esp.sh
@@ -0,0 +1,2 @@
+export LIBCLANG_PATH="/home/andrea/.espressif/tools/xtensa-esp32-elf-clang/esp-14.0.0-20220415-x86_64-unknown-linux-gnu/lib/"
+export PATH="/home/andrea/.espressif/tools/xtensa-esp32-elf-gcc/8_4_0-esp-2021r2-patch3-x86_64-unknown-linux-gnu/bin/:/home/andrea/.espressif/tools/xtensa-esp32s2-elf-gcc/8_4_0-esp-2021r2-patch3-x86_64-unknown-linux-gnu/bin/:/home/andrea/.espressif/tools/xtensa-esp32s3-elf-gcc/8_4_0-esp-2021r2-patch3-x86_64-unknown-linux-gnu/bin/:$PATH"
